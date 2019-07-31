c39cca6d5a8495db536462051100eb62d5196f12
Lou Montulli <lou@montulli.org>
2019-07-31 13:21:59 -0700

    Add missing paren

--- lib/jquery_query_builder/operators/contains.rb
+++ lib/jquery_query_builder/operators/contains.rb
@@ -8,7 +8,7 @@ module JqueryQueryBuilder
 
       def sql_query(id, value)
         # sanitize_sql_for_conditions is made public in Rails 5.2
-        ApplicationRecord.send(:sanitize_sql_for_conditions, ["(LOWER(#{id}) LIKE LOWER(?)", "%#{value}%"] )
+        ApplicationRecord.send(:sanitize_sql_for_conditions, ["(LOWER(#{id}) LIKE LOWER(?))", "%#{value}%"] )
       end
     end
   end
